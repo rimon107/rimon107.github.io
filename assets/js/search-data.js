9db4995f9e9b7a1895c93f6acb96188e64871b95
rimon107 <nahidulislam107@gmail.com>
2026-05-09 22:34:28 +0000

    Deploying to gh-pages from @ rimon107/rimon107.github.io@0b21342a57328304fb6ad0705d09f407c6fc30d8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,9 +23,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-curriculum-vitae",
+          title: "CURRICULUM VITAE",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
